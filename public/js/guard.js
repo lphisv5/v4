7edfacb5666970db2965bef63e20ddb6e725cd34
YANZ <165420181+lphisv5@users.noreply.github.com>
2025-12-17 22:27:27 +0700

    Create guard.js

---
+++ public/js/guard.js
@@ -0,0 +1,7 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from
+"https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+onAuthStateChanged(auth, user => {
+  if (!user) location.href = "/index.html";
+});
