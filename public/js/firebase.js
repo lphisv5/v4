bc0a3e9608477ef0a1e89ab1c23d9e520a4d6d72
YANZ <165420181+lphisv5@users.noreply.github.com>
2025-12-17 22:22:29 +0700

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,1 +1,14 @@
-b
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "yanzz-6e06c.firebaseapp.com",
+  databaseURL: "https://yanzz-6e06c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "yanzz-6e06c"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
